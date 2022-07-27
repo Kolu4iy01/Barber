eda48199c84756ea504422ce639bfe8ca845ff73
Nik <107045703+Kolu4iy01@users.noreply.github.com>
2022-07-27 10:30:23 +0300

    Add files via upload

--- Barber.py
+++ Barber.py
@@ -10,21 +10,25 @@ days_work = int(input(
 days_skip = int(input(
     'Сколько дней после этого отдыхает: '))
 
-# форматирование строчной даты в datetime
-date_start_iso = datetime.strptime(date_start, "%Y-%m-%d")
-date_end_iso = datetime.strptime(date_end, "%Y-%m-%d")
-# интервал между датами
-interval = days_work + days_skip
-# период составления расписания форматированный в int
-end = int(((date_end_iso - date_start_iso).__str__())[:2])
-# генерация массива с датами
-date_new_work = []
-for y in takewhile(lambda y: y < end, count(0, interval)):
-    if days_work > 0:    #range(0, end, interval):
-        delta = ((date_start_iso + timedelta(days=y)).__str__())[:10]
-    else:
-        break
-    date_new_work.append(delta)
-print(date_new_work)
+
+def data_interval(date_start, date_end, days_work, days_skip):
+        # форматирование строчной даты в datetime
+    date_start_iso = datetime.strptime(date_start, "%Y-%m-%d")
+    date_end_iso = datetime.strptime(date_end, "%Y-%m-%d")
+        # интервал между датами
+    interval = days_work + days_skip
+        # период составления расписания форматированный в int
+    end = int(((date_end_iso - date_start_iso).__str__())[:2])
+        # генерация массива с датами
+    date_new_work = []
+    for y in takewhile(lambda y: y < end, count(0, interval)):           # range(0, end, interval):
+        if days_work > 0:
+            delta = ((date_start_iso + timedelta(days=y)).__str__())[:10]
+        else:
+            break
+        date_new_work.append(delta)
+
+
+print(data_interval())
 
 
